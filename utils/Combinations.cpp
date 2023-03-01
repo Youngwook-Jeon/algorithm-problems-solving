065af59ab5af39eec9725bf5cd4f82c19a236368
Young wook Jeon <ela87@naver.com>
2023-03-01 23:50:51 +0900

    commit

---
+++ utils/Combinations.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+using namespace std;
+
+// Calculate nCk
+class Solution
+{
+public:
+  vector<vector<int>> ans;
+  vector<int> cur;
+
+  vector<vector<int>> combine(int n, int k)
+  {
+    dfs(n, k, 0);
+    return ans;
+  }
+
+  void dfs(int n, int k, int pos)
+  {
+    if (cur.size() == k)
+    {
+      ans.push_back(cur);
+      return;
+    }
+
+    for (int i = pos + 1; i <= n; i++)
+    {
+      cur.push_back(i);
+      dfs(n, k, i);
+      cur.pop_back();
+    }
+  }
+};
---
+++ utils/Permutations.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+using namespace std;
+
+// Find permutations of given array
+class Solution
+{
+public:
+  vector<vector<int>> ans;
+
+  vector<vector<int>> permute(vector<int> &nums)
+  {
+    backtrack(nums, 0);
+    return ans;
+  }
+
+  void backtrack(vector<int> &nums, int pos)
+  {
+    if (pos == nums.size())
+    {
+      ans.push_back(nums);
+      return;
+    }
+
+    for (int i = pos; i < nums.size(); i++)
+    {
+      swap(nums[pos], nums[i]);
+      backtrack(nums, pos + 1);
+      swap(nums[pos], nums[i]);
+    }
+  }
+};
