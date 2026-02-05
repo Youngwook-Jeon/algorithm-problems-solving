ede9a1183a13aed38bcbc3b952e3a1e0f6635e35
Youngwook-Jeon <ela87@naver.com>
2026-02-05 23:46:52 +0900

    commit

---
+++ leetcode/133_CloneGraph.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+class Solution {
+    unordered_map<int, Node*> mp;
+public:
+    Node* cloneGraph(Node* node) {
+        return solve(node);
+    }
+
+    Node* solve(Node* node) {
+        if (node == nullptr) return nullptr;
+        if (mp.find(node->val) != mp.end()) return mp[node->val];
+
+        Node* newNode = new Node(node->val);
+        mp[node->val] = newNode;
+        for (auto next : node->neighbors) {
+            Node* child = solve(next);
+            newNode->neighbors.push_back(child);
+        }
+        return newNode;
+
+    }
+};
